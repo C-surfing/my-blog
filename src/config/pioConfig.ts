127d8dd958f4d402cb72dd97dd576d33716fd17c
JiahongChen <13622484929@139.com>
2026-06-27 14:32:12 +0800

    test: revert to NOIR model for debugging

--- src/config/pioConfig.ts
+++ src/config/pioConfig.ts
@@ -1,12 +1,8 @@
 import type { PioConfig } from "../types/config";
 
-// Pio 看板娘配置
 export const pioConfig: PioConfig = {
 	enable: true,
-	models: [
-		"/pio/models/Pichu/Pichu.model3.json",
-		"https://model.hacxy.cn/cat-black/model.json",
-	],
+	models: ["/pio/models/NOIR/noir.model3.json"],
 	position: "left",
 	width: 280,
 	height: 250,
@@ -15,15 +11,10 @@ export const pioConfig: PioConfig = {
 	hideAboutMenu: false,
 	dialog: {
 		welcome: "Welcome to Csurfing's Agent Lab!",
-		touch: [
-			"Hey! That tickles!",
-			"Pika~?",
-			"Don't poke me!",
-			"I'm working on some RL papers!",
-		],
+		touch: ["Hey!", "Pika~?", "Don't poke me!"],
 		home: "Back to homepage?",
 		skin: ["Check this out!", "Looking good~"],
-		close: "See you! Come back for more agents!",
+		close: "See you!",
 		link: "https://github.com/C-surfing/my-blog",
 	},
 };
